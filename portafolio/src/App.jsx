d52e2aea8c72b6d21817fffabab00cb329ec4e0f
Agustin Rabadán Martínez <agusrabadan85@gmail.com>
2025-03-03 19:06:37 +0000

    inicio

---
+++ portafolio/src/App.jsx
@@ -0,0 +1,30 @@
+
+import reactLogo from './assets/react.svg'
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1>Agustin Rabadan</h1>
+      <h2>Full Stack Developer</h2>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/800px-Python-logo-notext.svg.png" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQru4uS0wYUibX4iBXMYXgiEWK6eYr5qikyZg&s" className="logo react" alt="React logo" />
+        </a>
+      </div>
+      
+      
+    </>
+  )
+}
+
+export default App
---
+++ portafolio/src/Redes.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './redes.css'
+
+
+
+export const Redes = () => {
+    return (
+        <div>
+            <div><img src="https://static.vecteezy.com/system/resources/previews/023/986/970/non_2x/linkedin-logo-linkedin-logo-transparent-linkedin-icon-transparent-free-free-png.png"></img></div>
+                
+        </div>
+    );
+  };
